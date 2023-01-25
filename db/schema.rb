decdbb36c25e43c23e8bc5c97f182af470516d4c
Tom Bakker <tom.bakker92@gmail.com>
2023-01-25 16:49:41 +0100

    Move to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_01_18_221520) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.string "content_type", null: false
-    t.integer "content_id", null: false
+    t.bigint "content_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["content_type", "content_id"], name: "index_activities_on_content"
